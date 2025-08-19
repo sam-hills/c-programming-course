076811fbbb4a3a05817719534e6f464d449d0e51
SamHills <167531150+sam-hills@users.noreply.github.com>
2025-08-18 20:51:36 -0400

    remove empty functions

--- include/parse.h
+++ include/parse.h
@@ -18,11 +18,6 @@ struct employee_t {
 
 int create_db_header(int fd, struct dbheader_t **headerOut);
 int validate_db_header(int fd, struct dbheader_t **headerOut);
-int read_employees(int fd, struct dbheader_t *,
-                   struct employee_t **employeesOut);
 int output_file(int fd, struct dbheader_t *dbhdr);
-void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees);
-int add_employee(struct dbheader_t *dbhdr, struct employee_t *employees,
-                 char *addstring);
 
 #endif
